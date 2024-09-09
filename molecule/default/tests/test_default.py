d654cc09303fcaa18bfa2cdc30b1ce267e31fd95
pako-23 <pasqualepolverino98@gmail.com>
2024-09-09 09:53:14 +0200

    first commit

---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,10 @@
+import os
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ["MOLECULE_INVENTORY_FILE"]
+).get_hosts("github-runner")
+
+
+def test_github_runner_executables(host):
+    pass
